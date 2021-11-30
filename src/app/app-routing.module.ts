54dfc6d53a5ae5153bdb6f2d31cf7158ecbccae8
Oli <12577818+oliwhere@users.noreply.github.com>
2021-11-30 16:45:31 +0100

    Routing implementiran

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: AppComponent
+  },
+  {
+    path: "**",
+    redirectTo: "/home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
