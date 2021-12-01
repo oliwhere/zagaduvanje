8ebdf2227b6d8ed11601fc4515562598f130d9e9
Oli <12577818+oliwhere@users.noreply.github.com>
2021-12-01 12:46:29 +0100

    Celosno implementirani se rutite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
-import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home",
+  },
+  {
     path: "home",
-    component: AppComponent
+    component: HomeComponent
   },
   {
     path: "**",
-    redirectTo: "/home"
+    redirectTo: "home"
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,7 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
