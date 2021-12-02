fca1bf4a91c9a87dcb576e75ea015897929b3475
bojan <bojan.angjelkovski@cosmicdevelopment.com>
2021-12-02 12:15:49 +0100

    Added filter by name in about us page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,6 +9,7 @@ import { catchError, map, Observable, of } from 'rxjs';
 })
 export class AboutComponent implements OnInit {
   teamMembers: any[] = [];
+  filteredTeamMembers: any[] = [];
   constructor() {
   }
 
@@ -17,39 +18,58 @@ export class AboutComponent implements OnInit {
       {
         name: 'Nikola',
         img: '../assets/images/nikola.jpg',
-        info: 'Basic member info',
+        info: 'Premium member',
+        donations: 12,
         linkedInUrl: 'http://linkiedin/nikola'
       },
       {
-        name: 'Nikola',
-        img: '../assets/images/nikola.jpg',
-        info: 'Basic member info',
-        linkedInUrl: 'http://linkiedin/nikola'
+        name: 'Denica',
+        img: '../assets/images/denica.jpeg',
+        info: 'Premium member',
+        donations: 32,
+        linkedInUrl: 'http://linkiedin/denica'
+      },
+      {
+        name: 'Oliver',
+        img: '../assets/images/oliver.jpeg',
+        info: 'Basic member',
+        donations: 10,
+        linkedInUrl: 'http://linkiedin/oliver'
+      },
+      {
+        name: 'Simona',
+        img: '../assets/images/simona.jpeg',
+        info: 'Basic member',
+        donations: 1,
+        linkedInUrl: 'http://linkiedin/simona'
+      },
+      {
+        name: 'Petar',
+        img: '../assets/images/petar.jpeg',
+        info: 'Basic member',
+        donations: 11,
+        linkedInUrl: 'http://linkiedin/petar'
       },
       {
         name: 'Nikola',
-        img: '../assets/images/nikola.jpg',
-        info: 'Basic member info',
-        linkedInUrl: 'http://linkiedin/nikola'
-      },
-      {
-        name: 'Nikola',
-        img: '../assets/images/nikola.jpg',
-        info: 'Basic member info',
-        linkedInUrl: 'http://linkiedin/nikola'
-      },
-      {
-        name: 'Nikola',
-        img: '../assets/images/nikola.jpg',
-        info: 'Basic member info',
-        linkedInUrl: 'http://linkiedin/nikola'
-      },
-      {
-        name: 'Nikola',
-        img: '../assets/images/nikola.jpg',
-        info: 'Basic member info',
+        img: '../assets/images/elena.jpeg',
+        info: 'Premium member',
+        donations: 6,
         linkedInUrl: 'http://linkiedin/nikola'
       }
     ]
+    this.filteredTeamMembers = this.teamMembers;
+  }
+
+  public applyFilter(value: any): boolean {
+    if (!value || value.length === 0) {
+      this.filteredTeamMembers = this.teamMembers;
+      return false;
+    }
+    this.filteredTeamMembers = this.teamMembers.filter( (member: any) => {
+      return member.name.toLowerCase().includes(value.toLowerCase());
+    });
+
+    return true;
   }
 }
