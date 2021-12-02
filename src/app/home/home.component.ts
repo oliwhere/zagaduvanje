0500bddd8ffabe18ca25a390d762a1e7cb413a84
Oli <12577818+oliwhere@users.noreply.github.com>
2021-12-02 22:25:57 +0100

    Fixed the position of the cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
 import { HomeService } from './home.service';
 import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
@@ -13,35 +13,33 @@ import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 export class HomeComponent implements OnInit, AfterViewInit {
   map: any;
   @ViewChild('map') mapElement: any;
-  lat = 43.879078;
-  lng = -103.4615581;
+  lat = 41.996505;
+  lng = 21.4359786;
   markers = [
-    { lat: 22.33159, lng: 105.63233 },
-    { lat: 7.92658, lng: -12.05228 },
-    { lat: 48.75606, lng: -118.859 },
-    { lat: 5.19334, lng: -67.03352 },
-    { lat: 12.09407, lng: 26.31618 },
-    { lat: 47.92393, lng: 78.58339 }
+    { lat: 41.9892813, lng: 21.467238 },
+    { lat: 41.9774968, lng: 21.475185 },
+    { lat: 41.9863011, lng: 21.402898 },
+    { lat: 42.0034074, lng: 21.3988385 },
+    { lat: 42.004239, lng: 21.4030181 },
+    { lat: 42.004239, lng: 21.4030181 },
   ];
   volunteers: object[] = [];
   apiLoaded: Observable<boolean>;
-  center: google.maps.LatLngLiteral;
-  options: google.maps.MapOptions = {
-    zoom: 15,
-    disableDefaultUI: true,
-  };
 
   ngAfterViewInit(): void {
     const mapProperties = {
       center: new google.maps.LatLng(this.lat, this.lng),
-      zoom: 2,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      zoom: 13.25,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
-    this.markers.forEach(location => {
+    this.map = new google.maps.Map(
+      this.mapElement.nativeElement,
+      mapProperties
+    );
+    this.markers.forEach((location) => {
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(location.lat, location.lng),
-        map: this.map
+        map: this.map,
       });
     });
   }
