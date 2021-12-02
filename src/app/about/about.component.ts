c692693d0458ddb130c3dd992f81a6aa3871de10
Oli <12577818+oliwhere@users.noreply.github.com>
2021-12-02 23:37:03 +0100

    Fixed text

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,58 +10,42 @@ import { catchError, map, Observable, of } from 'rxjs';
 export class AboutComponent implements OnInit {
   teamMembers: any[] = [];
   filteredTeamMembers: any[] = [];
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
     this.teamMembers = [
       {
-        name: 'Nikola',
-        img: '../assets/images/sliki/nikola.jpg',
+        name: 'Oliver',
+        img: '../assets/images/sliki/oli.jpg',
         info: 'Premium member',
         donations: 12,
-        label:'Create & designin About page',
-
-        linkedInUrl: 'https://www.linkedin.com/in/nikola-ivanchov-243479161/'
+        label: 'Create & designin Home page and implement map',
+        linkedInUrl: 'https://www.linkedin.com/in/oliver-andonovic-5363a0213/',
       },
       {
         name: 'Denica',
         img: '../assets/images/sliki/denica.jpg',
         info: 'Premium member',
         donations: 32,
-        label:'Create & designin Forms',
-        linkedInUrl: 'http://linkiedin/denica'
+        label: 'Create & designin Forms',
+        linkedInUrl: 'http://linkiedin/denica',
       },
       {
-        name: 'Oliver',
-        img: '../assets/images/sliki/oli.jpg',
+        name: 'Nikola',
+        img: '../assets/images/sliki/nikola.jpg',
         info: 'Basic member',
         donations: 10,
-        label:'Create & designin Home page and implement map',
-        linkedInUrl: 'https://www.linkedin.com/in/oliver-andonovic-5363a0213/'
+        label: 'Create & designin About page',
+        linkedInUrl: 'https://www.linkedin.com/in/nikola-ivanchov-243479161/',
       },
       {
         name: 'Simona',
         img: '../assets/images/sliki/sims.jpg',
         info: 'Basic member',
         donations: 1,
-        linkedInUrl: 'http://linkiedin/simona'
-      },{
-        name: 'Jovana',
-        img: '../assets/images/sliki/sims.jpg',
-        info: 'Basic member',
-        donations: 1,
-        linkedInUrl: 'http://linkiedin/jovana'
-      },{
-        name: 'Ivan',
-        img: '../assets/images/sliki/sims.jpg',
-        info: 'Basic member',
-        donations: 1,
-        linkedInUrl: 'http://linkiedin/ivan'
-      }
-
-
-    ]
+        linkedInUrl: 'http://linkiedin/simona',
+      },
+    ];
     this.filteredTeamMembers = this.teamMembers;
   }
 
@@ -70,7 +54,7 @@ export class AboutComponent implements OnInit {
       this.filteredTeamMembers = this.teamMembers;
       return false;
     }
-    this.filteredTeamMembers = this.teamMembers.filter( (member: any) => {
+    this.filteredTeamMembers = this.teamMembers.filter((member: any) => {
       return member.name.toLowerCase().includes(value.toLowerCase());
     });
 
