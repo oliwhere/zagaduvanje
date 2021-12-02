7283550e30d86f5abcb0d5ed3c4fd160f1c811dc
bojan <bojan.angjelkovski@cosmicdevelopment.com>
2021-12-02 21:52:54 +0100

    Added map to home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
 import { HomeService } from './home.service';
 
@@ -9,13 +9,40 @@ import { HomeService } from './home.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  map: any;
+  @ViewChild('map') mapElement: any;
+  lat = 43.879078;
+  lng = -103.4615581;
+  markers = [
+    { lat: 22.33159, lng: 105.63233 },
+    { lat: 7.92658, lng: -12.05228 },
+    { lat: 48.75606, lng: -118.859 },
+    { lat: 5.19334, lng: -67.03352 },
+    { lat: 12.09407, lng: 26.31618 },
+    { lat: 47.92393, lng: 78.58339 }
+  ];
   volunteers: object[] = [];
   apiLoaded: Observable<boolean>;
   options: google.maps.MapOptions = {
     center: { lat: 41.9991487, lng: 21.3898709 },
     zoom: 15,
   };
+
+  ngAfterViewInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(this.lat, this.lng),
+      zoom: 2,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.markers.forEach(location => {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location.lat, location.lng),
+        map: this.map
+      });
+    });
+  }
 
   click(event: google.maps.MapMouseEvent) {
     console.log(event);
